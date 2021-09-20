d431a29bb35317d3b0c928ae36fd5f666c7096b4
nikola <gooodini@gmail.com>
2021-09-20 17:10:40 +0200

    Adds documentation and exports functions

--- R/functions.R
+++ R/functions.R
@@ -1,25 +1,38 @@
-# Useful default settings for very small tabular output.
+#' Useful default settings for very small tabular output.
+#' @export
 tiny.datatable <- function(DT, rownames=FALSE, options=list(searching=FALSE, info=FALSE, paging=FALSE, ordering=FALSE)) {
   library(DT)
   datatable(DT, rownames=rownames, options=options)
 }
 
-# Datatable that hides the rownames and adds Download CSV button
+#' Datatable that hides the rownames and adds Download CSV button
+#' @export
 datatable.with.csv <- function(DT, rownames=FALSE, ...) {
   library(DT)
   datatable(DT, rownames=rownames, extensions='Buttons', options=list(dom='Bfrtip', buttons='csv'))
 }
 
-# Wrapper around length(unique()). The naming is influenced by nunique in pandas and in plyr, but shortend to nuniq like the UNIX tool.
+#' Wrapper around length(unique()). The naming is influenced by nunique in pandas and in plyr, but shortend to nuniq like the UNIX tool.
+#' @export
 nuniq <- function(...) {
   length(unique(...))
 }
 
-# Wrapper around ggplotly that hides the annoying Mode Bar options
+#' Wrapper around ggplotly that hides the annoying Mode Bar options
+#' @export
 .ggplotly <- function(...) {
   ggplotly(..., dynamicTicks=FALSE) %>% config(displayModeBar = FALSE)
 }
 
+#' If you have a table like:
+#'
+#' variant   | cr
+#' TREATMENT | 5.8
+#' CONTROL   | 3.7
+#'
+#' This function can calculate the uplift in TREATMENT using abtest.uplift(dat, 'cr').
+#'
+#' @export
 abtest.uplift <- function(summary_dt, column) {
   if (! column %in% names(summary_dt)) {
     stop('abtest_calculate_uplift data.table is expected to have a variant column')
